40a5f3698b76e64e3ee9e45cfd02578aac412c88
qubiqeltd <qubiqetechnologies@gmail.com>
2026-09-24 21:25:40 +0600

    output: export

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
+  output: "export",
   /* optional: set basePath or images configuration if hosting on GitHub Pages, S3, etc. */
   // basePath: "/subfolder",
   // images: { unoptimized: true },
